e605042fcf1fc446a9f07f348ad3650a1e89f819
Coultonf <cjrfraser@gmail.com>
2017-03-27 23:01:32 -0600

    Worked on the authentication using the scotch io tutorial

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mongodb = require('mongodb'),
 
 MongoClient = mongodb.MongoClient,
 
-url = 'mongodb://localhost:27017/good_things',
+url = 'mongodb://localhost:27017/Good_Things',
 
 mongoose = require('mongoose'),
 
@@ -27,6 +27,10 @@ passport = require('passport'),
 bitString = require('bitstring'),
 
 session = require('express-session'),
+
+flash = require('connect-flash'),
+
+morgan = require('morgan'),
 
 bits = new bitString(),
 
@@ -48,135 +52,142 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use(bodyParser.json());
+
 app.use(cookieParser());
 
 app.use(helmet());
 
 // required for passport
 app.use(session({ secret: bits.writebits(20),
-                resave: false,
-                saveUninitialized: false })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+    resave: false,
+    saveUninitialized: false })); // session secret
+    //see ./src/passport/passport.js for the passport code
+    //require('./src/passport/passport')(passport);
+
+    app.use(morgan('dev')); // log every request to the console
+
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+
+    mongoose.connect(url, function(error) {
+        if(error){
+            console.log(error);
+        }
+        else {
+            console.log("Connection made at: " +url);
+        }
+    });
+
+    app.use(flash());
 
 
-mongoose.connect(url, function(error) {
-  if(error){
-    console.log(error);
-  }
-  else {
-    console.log("Connection made at: " +url);
-  }
-});
+    // define the schema for our user model
+    // var userSchema = mongoose.Schema({
+    //
+    //     local            : {
+    //         email        : String,
+    //         password     : String,
+    //     }
+    //   });
 
-// define the schema for our user model
-var userSchema = mongoose.Schema({
+    // methods ======================
+    // generating a hash
+    // userSchema.methods.generateHash = function(password) {
+    //     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+    // };
+    //
+    // // checking if password is valid
+    // userSchema.methods.validPassword = function(password) {
+    //     return bcrypt.compareSync(password, this.local.password);
+    // };
 
-    local            : {
-        email        : String,
-        password     : String,
-    }
-  });
+    // create the model for users and expose it to our app
+    //module.exports = mongoose.model('User', userSchema);
 
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+    homeRouter.route('/').get(function(req, res){
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
+        res.sendFile(__dirname + '/home-page.html');
 
-// create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+    });
 
-homeRouter.route('/').get(function(req, res){
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
-    res.sendFile(__dirname + '/index.html');
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
-});
+    app.get('/profile', function(req, res) {
+        res.send('Profile');
+    });
 
-loginRouter.route('/').post(function(req, res){
 
-    console.log(req.body.email);
-    console.log(req.body.password);
+    signinRouter.route('/').get(function(req, res){
 
-    //authenticate the user using passport here...
-    if (true){
+        res.sendFile(__dirname + '/home-page.html');
 
-        res.sendFile(__dirname + '/secure/user-page.html');
+    });
 
-    }
-    else{
+    signupRouter.route('/').get(function(req, res){
 
-        res.send('index.html');
+        res.sendFile(__dirname + '/home-page.html');
 
-    }
+    });
 
-});
+    landingPageRouter.route('/').get(function(req, res){
 
-signinRouter.route('/').get(function(req, res){
+        res.sendFile(__dirname + '/home-page.html');
 
-    res.sendFile(__dirname + '/home-page.html');
+    });
 
-});
+    businessInfoRouter.route('/').get( function(req, res){
 
-signupRouter.route('/').get(function(req, res){
+        res.sendFile(__dirname + '/home-page.html');
 
-    res.sendFile(__dirname + '/home-page.html');
+    });
 
-});
+    app.get('/', function(req, res){
 
-landingPageRouter.route('/').get(function(req, res){
+        //TODO: authenticate token if not valid or null
 
-    res.sendFile(__dirname + '/home-page.html');
+        if(false)
 
-});
+        res.sendFile(__dirname + '/home-page.html');
 
-businessInfoRouter.route('/').get( function(req, res){
-
-    res.sendFile(__dirname + '/home-page.html');
-
-});
-
-app.get('/', function(req, res){
-
-//TODO: authenticate token if not valid or null
-
-    if(false)
-
-        res.sendFile(__dirname + '/index.html');
-
-    else
+        else
         //TODO: create sign in page
         res.sendFile(__dirname + '/home-page.html');
 
-});
+    });
 
-app.use('/login', loginRouter);
+    app.use('/login', loginRouter);
 
-app.use('/home', homeRouter);
+    app.use('/home', homeRouter);
 
-app.use('/signin', signinRouter);
+    app.use('/signin', signinRouter);
 
-app.use('/', landingPageRouter);
+    app.use('/', landingPageRouter);
 
-app.use('/signup', signupRouter);
+    app.use('/signup', signupRouter);
 
-app.use('/business', businessInfoRouter);
+    app.use('/business', businessInfoRouter);
 
-app.listen(port, function(err){
+    app.listen(port, function(err){
 
-    if(err){
+        if(err){
 
-        console.log('Error: ' + err);
+            console.log('Error: ' + err);
 
-    }
+        }
 
-    else
+        else
 
         console.log('Server started at localhost:' + port);
 
-});
+    });
