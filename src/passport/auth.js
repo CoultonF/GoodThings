be3ff72f2ae44429d6866654ce938464d7f1aa07
JonathanCates <catesjj94@gmail.com>
2017-04-09 15:44:58 -0600

    Google oAuth working

--- src/passport/auth.js
+++ src/passport/auth.js
@@ -3,7 +3,7 @@ module.exports = {
 
     'googleAuth' : {
         'clientID'      :  '21040107286-7f8scbae9dqt5pg7g250vf91od0fo9ef.apps.googleusercontent.com',
-        'clientSecret'  : ' fkT5agG2CxMyrTYIAgNCa3-v',
+        'clientSecret'  : 'fkT5agG2CxMyrTYIAgNCa3-v',
         'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     }
 
