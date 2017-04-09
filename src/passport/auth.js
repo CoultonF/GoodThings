90f45a511245e98e6b755cc045c8aa6623a35c96
JonathanCates <catesjj94@gmail.com>
2017-04-09 15:40:06 -0600

    Google oAuth parse error

---
+++ src/passport/auth.js
@@ -0,0 +1,10 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      :  '21040107286-7f8scbae9dqt5pg7g250vf91od0fo9ef.apps.googleusercontent.com',
+        'clientSecret'  : ' fkT5agG2CxMyrTYIAgNCa3-v',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
